333ebf636679d7e9ec147bb7d8e42b64e66fad9c
jrabbit <jackjrabbit@gmail.com>
2016-04-22 15:08:42 -0400

    setup file to being work on #1

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+setup(name="stature",
+      version="0.1.0a1",
+      scripts=['stature.py'],
+      author="Jack Laxson",
+      author_email="jackjrabbit@gmail.com",
+      description="Docker container metadata as a Cachet component data!",
+      license="AGPL v3+",
+      install_requires=["toml", "baker==1.3", "cachet==0.1.0", "docker-py>=1.8.0"],
+      url="https://github.com/Colorless-Green-Ideas/docker-stature",
+      classifiers=["License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+      'Intended Audience :: Developers',
+      'Topic :: Utilities','Topic :: System :: Monitoring'])
